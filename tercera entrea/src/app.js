4beb182fd584d108a9494721aa8acd49a5ca1bc3
ivan navatta <ivannavatta@gmail.com>
2023-12-07 02:48:43 -0300

    tercera entrega

---
+++ tercera entrea/src/app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import ProductManager from "./productManager.js";
+
+const PORT = 3000;
+const app = express();
+const productManager = new ProductManager();
+
+
+
+app.listen(PORT, () => {
+    console.log(`servidor funcionando en el puerto: ${PORT}`);
+});
+
+app.get('/products', async (req, res) => {
+    try{
+        const products = await productManager.consultarProductos();
+
+        const limit = parseInt(req.query.limit)
+
+        if (!isNaN(limit) && limit > 0) {
+            const productosFiltrados = products.slice(0, limit);
+            res.json({ products: productosFiltrados });
+        } else {
+            res.json({ products });
+        }
+    } catch (error) {
+        console.error('Error al obtener productos:', error);
+        res.status(500).json({ error: 'Error al obtener productos' });
+    }
+    
+})
+
+app.get('/products/:id', async (req, res) => {
+    const idProducto = req.params.id; // Cambiado de req.params.idProducto a req.params.id
+    try {
+        const producto = await productManager.getProductById(idProducto);
+        if (!producto) {
+            return res.status(404).json({ error: 'Producto no encontrado' });
+        }
+        res.json({ producto });
+    } catch (error) {
+        console.error('Error al obtener producto por ID:', error);
+        res.status(500).json({ error: 'Error al obtener producto por ID' });
+    }
+});
+
+   
+   
